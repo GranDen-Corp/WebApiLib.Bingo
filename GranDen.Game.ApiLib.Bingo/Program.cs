1f9f046643f06c0711d90f81fefa6ba9d6456a0c
Chen-Yu Pao <windperson@gmail.com>
2020-06-11 13:10:09 +0800

    update git ignore

---
+++ GranDen.Game.ApiLib.Bingo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GranDen.Game.ApiLib.Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
